f69987b1153992f872460aca4e949aef690457e6
vmois <vladyslav.moisieienkov@gmail.com>
2017-02-06 14:48:06 +0100

    feat: Add startup item to location, add locations

--- js/resources.js
+++ js/resources.js
@@ -17,7 +17,19 @@ var Items = {
         item_11 = new Item(11, 'an AXE', 1, 'AXE');
         item_12 = new Item(12, 'STICKS', 1, 'STICKS');
         item_13 = new Item(13, 'sheeplegs', 0, 'sheeplegs');
+        item_14 = new Item(14, 'MUSHROOMS', 1, 'MUSHROOMS');
+        item_15 = new Item(15, 'MONEY', 1, 'MONEY');
+        item_16 = new Item(16, 'a BARREL', 1, 'BARREL');
+        item_17 = new Item(17, 'a sheeptrunk', 0, 'sheeptrunk');
+        item_18 = new Item(18, 'BERRIES', 1, 'BERRIES');
+        item_19 = new Item(19, 'WOOL', 1, 'WOOL');
+        item_20 = new Item(20, 'a sheepskin', 0, 'sheepskin');
         item_21 = new Item(21, 'a BAG', 1, 'BAG');
+        item_24 = new Item(24, 'a SPADE', 1, 'SPADE');
+        item_27 = new Item(27, 'a BUCKET', 1, 'BUCKET');
+        item_31 = new Item(31, 'a STONE', 1, 'STONE');
+        item_32 = new Item(32, 'a FISH', 1, 'FISH');
+        item_33 = new Item(33, 'a KNIFE', 1, 'KNIFE');
     }
 };
 
@@ -41,21 +53,45 @@ var Locations = {
         };
 
         // test location (YX)
-        location_47 = new Location("47.gif", "You are in a frontyard of your house",
-                                   "rgb(255,190,99)", [true, false, true, true]);
-        location_46 = new Location("46.gif", "You are on a town street",
-                                   "rgb(255,190,99)", [true, true, false, true]);
-        location_45 = new Location("45.gif", "You are at a main crossroad",
-                                   "rgb(255,190,99)", [true, true, true, true]);
+        location_13 = new Location("13.gif", "A hill",
+                                   "rgb(117,237,243)", [false, true, true, true],
+                                   [item_31]);
+        location_15 = new Location("15.gif", "An old deserted hut",
+                                   "rgb(220,204,61)", [false, true, false, true],
+                                   [item_27]);
+        location_17 = new Location("17.gif", "A dark forest",
+                                   "rgb(140,253,99)", [false, false, true, true],
+                                   [item_14]);
+        location_23 = new Location("23.gif", "You are by a roadside shrine",
+                                   "rgb(167,245,63)", [true, true, true, true],
+                                   [item_10]);
+        location_27 = new Location("27.gif", "You are in a deep forest",
+                                   "rgb(140,253,99)", [true, false, false, true],
+                                   [item_18]);
+        location_32 = new Location("32.gif", "You are by the Vistula River",
+                                   "rgb(140,214,255)", [true, false, false, true],
+                                   [item_32]);
+        location_36 = new Location("36.gif", "You are in a butcher's shop",
+                                   "rgb(255,188,102)", [false, false, true, false]);
+        location_37 = new Location("37.gif", "You are in a cooper's house",
+                                   "rgb(255,188,102)", [false, false, true, false]);
         location_44 = new Location("44.gif", "You are by the water mill",
                                    "rgb(255,190,99)", [false, true, false, false],
                                   [item_21]);
-        location_37 = new Location("37.gif", "You are in a cooper's house",
-                                   "rgb(255,188,102)", [false, false, true, false]);
-        location_36 = new Location("36.gif", "You are in a butcher's shop",
-                                   "rgb(255,188,102)", [false, false, true, false]);
+        location_45 = new Location("45.gif", "You are at a main crossroad",
+                                   "rgb(255,190,99)", [true, true, true, true]);
+        location_46 = new Location("46.gif", "You are on a town street",
+                                   "rgb(255,190,99)", [true, true, false, true]);
+        location_47 = new Location("47.gif", "You are in a frontyard of your house",
+                                   "rgb(255,190,99)", [true, false, true, true]);
+        location_55 = new Location("55.gif", "You are on a street leading forest",
+                                   "rgb(255,190,99)", [true, false, true, true],
+                                   [item_33]);
         location_57 = new Location("57.gif", "You are in a shoemaker's house",
                                    "rgb(254,194,97)", [true, false, false, false]);
+        location_64 = new Location("64.gif", "You are in a bleak funeral house",
+                                   "rgb(254,194,97)", [false, true, false, false],
+                                   [item_24]);
 
 
 
