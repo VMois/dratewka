4450f67cd9bf84a5e45b028686db63ea5d87866c
vmois <vladyslav.moisieienkov@gmail.com>
2017-02-22 10:57:47 +0100

    feat: Default uppercase letters and change via caps lock and shift

--- js/UI.js
+++ js/UI.js
@@ -1,6 +1,7 @@
 var UI = {
     init: function()
     {
+        var is_upper_case = true;
         var mainInput = document.getElementById("main-input");
         // always foxus on main-input field
         mainInput.addEventListener("blur", function(){
@@ -15,7 +16,15 @@ var UI = {
             mainInput.value = mainInput.value.toUpperCase();
         })
 
+        document.addEventListener('keyup', (event) => {
+            if(event.keyCode == 16)
+            {
+                is_upper_case = !is_upper_case;
+            }
+        });
+
         document.addEventListener('keydown', (event) => {
+            event.preventDefault();
             if( !is_halt )
             {
                 const keyName = event.key;
@@ -26,6 +35,31 @@ var UI = {
 
                     // execute command
                     command_processor(command);
+                }
+
+                // accept only chracters (according to Unicode)
+                if(event.keyCode >= 65 && event.keyCode <= 90)
+                {
+                    if(is_upper_case)
+                    {
+                        mainInput.value += event.key.toUpperCase();
+                    }
+                    else
+                    {
+                        mainInput.value += event.key.toLowerCase();
+                    }
+                }
+
+                // backspace
+                if(event.keyCode == 8)
+                {
+                    mainInput.value = mainInput.value.slice(0, mainInput.value.length-1);
+                }
+
+                // caps lock and shift key press check
+                if(event.keyCode == 20 || event.keyCode == 16)
+                {
+                    is_upper_case = !is_upper_case;
                 }
             }
         }, false);
